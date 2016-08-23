702e4d1d2af30bc1ff602fbb8ed6af66f6f6573d
neo_jou <neo_jou@realtek.com>
2016-08-23 15:30:13 +0800

    exam 1 :
       LED

--- exam1/src/main.c
+++ exam1/src/main.c
@@ -9,13 +9,17 @@ void board_init(void)
 
 int main(void)
 {
+    SystemInit();
+
+    SystemCoreClockUpdate();
+
     board_init();
 
     gpio_init();
 
     gpio_set_dap_led(1);
-    gpio_set_cdc_led(1);
-    gpio_set_msd_led(1);
+    gpio_set_cdc_led(0);
+    gpio_set_msd_led(0);
 
     while(1);
     return 0;
